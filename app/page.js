f696d34c5c327e849740549cd8af3d62aafbf089
Rupen Bhimani <rupenbhimani109@gmail.com>
2023-12-03 16:21:52 +0530

    solve the deployment error

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import homeImg from "../public/homeImg.jpg";
-import homeImg3 from "../public/homeImg3.jpg";
+import homeImg3 from "../public/HomeImg3.jpg";
 import homeImg4 from "../public/homeImg4.jpg";
 import homeImg5 from "../public/homeImg5.jpg";
 import homeImg6 from "../public/homeImg6.jpg";
 import homeImg7 from "../public/homeImg7.jpg";
-import homeImg8 from "../public/homeImg8.jpg";
+import homeImg8 from "../public/homeimg8.jpg";
 import homeImg9 from "../public/homeImg9.jpg";
 import homeImg10 from "../public/homeImg10.jpg";
 
@@ -67,7 +67,7 @@ export default function Home() {
           {/* <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6 px-20">
             <div className="p-4 md:w-1/3 flex">
               <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-orange-300 text-indigo-500 mb-4 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
                   <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                 </svg>
               </div>
@@ -75,7 +75,7 @@ export default function Home() {
                 <h2 className="text-gray-900 text-2xl title-font font-medium mb-2">Shooting Stars</h2>
                 <p className="leading-relaxed text-lg">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge tilde.</p>
                 <a className="mt-3 text-orange-300 inline-flex items-center">Learn More
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
                 </a>
@@ -83,7 +83,7 @@ export default function Home() {
             </div>
             <div className="p-4 md:w-1/3 flex">
               <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-orange-300 text-indigo-500 mb-4 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
                   <circle cx="6" cy="6" r="3"></circle>
                   <circle cx="6" cy="18" r="3"></circle>
                   <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
@@ -93,7 +93,7 @@ export default function Home() {
                 <h2 className="text-gray-900 text-2xl title-font font-medium mb-2">The Catalyzer</h2>
                 <p className="leading-relaxed text-lg">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge tilde.</p>
                 <a className="mt-3 text-orange-300 inline-flex items-center">Learn More
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
                 </a>
@@ -101,7 +101,7 @@ export default function Home() {
             </div>
             <div className="p-4 md:w-1/3 flex">
               <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-orange-300 text-indigo-500 mb-4 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
                   <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                   <circle cx="12" cy="7" r="4"></circle>
                 </svg>
@@ -110,7 +110,7 @@ export default function Home() {
                 <h2 className="text-gray-900 text-2xl title-font font-medium mb-2">Neptune</h2>
                 <p className="leading-relaxed text-lg">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge tilde.</p>
                 <a className="mt-3 text-orange-300 inline-flex items-center">Learn More
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
                 </a>
@@ -291,7 +291,7 @@ export default function Home() {
             <div className="flex flex-wrap -m-4 text-center lg:px-12 lg:pb-16">
               <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
                 <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
                     <path d="M8 17l4 4 4-4m-4-5v9"></path>
                     <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
                   </svg>
@@ -301,7 +301,7 @@ export default function Home() {
               </div>
               <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
                 <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
                     <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
                     <circle cx="9" cy="7" r="4"></circle>
                     <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
@@ -312,7 +312,7 @@ export default function Home() {
               </div>
               <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
                 <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
                     <path d="M3 18v-6a9 9 0 0118 0v6"></path>
                     <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
                   </svg>
@@ -322,7 +322,7 @@ export default function Home() {
               </div>
               <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
                 <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
                     <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                   </svg>
                   <h2 className="title-font font-medium text-3xl text-gray-900">Fab english</h2>
@@ -333,7 +333,7 @@ export default function Home() {
 
               <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
                 <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
                     <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                   </svg>
                     <h2 className="title-font font-medium text-3xl text-gray-900">Fab english</h2>
@@ -342,7 +342,7 @@ export default function Home() {
               </div>
               <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
                 <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-orange-300 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
                     <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                   </svg>
                     <h2 className="title-font font-medium text-3xl text-gray-900">Fab english</h2>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -144,7 +144,7 @@ export default function Example() {
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
                                 <div className="flex flex-shrink-0 items-center gap-x-3">
-                                    <Image width={50} height={40} className="h-auto w-auto" src={logo} alt="Your Company" />
+                                    <Image width={50} height={40} className="" src={logo} alt="Your Company" />
                                     <p className="sm:text-2xl text-4xl font-bold">Orato</p>
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
